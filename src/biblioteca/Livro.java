c06936a9f619242f9a1b046ac363f8d09a5ed773
USER <blancasuarz@gmail.com>
2024-11-08 13:41:00 -0300

    Criando Classe Livro

---
+++ src/biblioteca/Livro.java
@@ -0,0 +1,29 @@
+package biblioteca;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private boolean disponibilidade;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponibilidade = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+  public boolean isDisponibilidade() {
+        return disponibilidade;
+  }
+
+  public void alterarDisponilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+  }
+  
+}
+
+
