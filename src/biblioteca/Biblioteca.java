da8e88a27145536514d93eaea9dc67c1d1880d59
USER <blancasuarz@gmail.com>
2024-11-11 18:07:55 -0300

    Criando Biblioteca

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,4 +1,34 @@
 package biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+
+    private List<Livro> acervo;
+    private List<Usuario> usuarios;
+
+    public Biblioteca() {
+        this.acervo = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        acervo.add(livro);
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public Livro buscarLivro(String titulo) {
+        return acervo.stream()
+                .filter(livro -> livro.getTitulo().equalsIgnoreCase(titulo))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
+
+
 }
